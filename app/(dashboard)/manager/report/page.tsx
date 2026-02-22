bdb0647afc4f341d93b73ecc9d276b06330a9267
hasansujon786 <hasansujon786@gmail.com>
2026-02-22 22:42:33 +0600

    feat(manager): update Pagination

--- app/(dashboard)/manager/report/page.tsx
+++ app/(dashboard)/manager/report/page.tsx
@@ -1,5 +1,6 @@
 import { ReportManagementColumns, demoReportData } from '@/components/columns/ReportsManagement'
 import SharedPropertyCardListActions from '@/components/pages/Viewer/SharedPropertyCardListActions/SharedPropertyCardListActions'
+import Pagination from '@/components/reusable/Pagination/Pagination'
 import SectionCard from '@/components/reusable/SectionCard/SectionCard'
 import CustomTable from '@/components/reusable/table/CustomTable'
 
@@ -30,6 +31,8 @@ export default function page() {
             roundedClass="rounded-lg"
           />
         </div>
+
+        <Pagination showHomeAndEnd={false} className="justify-start" size="icon-xs" />
       </SectionCard>
     </div>
   )
--- components/reusable/Pagination/Pagination.tsx
+++ components/reusable/Pagination/Pagination.tsx
@@ -2,45 +2,58 @@ import { ChevronsLeft, ChevronsLeftDouble } from '@/components/icons/Chevrons'
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 
-interface PaginationProps extends React.ComponentProps<'div'> {}
+interface PaginationProps extends React.ComponentProps<'div'> {
+  size?: React.ComponentProps<typeof Button>['size']
+  showHomeAndEnd?: boolean
+}
 
-export default function Pagination({ className }: PaginationProps) {
+export default function Pagination({
+  className,
+  size = 'icon-lg',
+  showHomeAndEnd = true,
+}: PaginationProps) {
   return (
     <div className={cn('flex justify-center gap-3', className)}>
-      <Button className="text-sm font-semibold" size="icon-lg" variant="muted">
-        <ChevronsLeftDouble className="size-5" />
-      </Button>
-      <Button className="text-sm font-semibold" size="icon-lg" variant="muted">
+      {showHomeAndEnd ? (
+        <Button className="text-sm font-semibold" size={size} variant="muted">
+          <ChevronsLeftDouble className="size-5" />
+        </Button>
+      ) : null}
+      <Button className="text-sm font-semibold" size={size} variant="muted">
         <ChevronsLeft className="size-5" />
       </Button>
-      <Button className="text-sm font-semibold" size="icon-lg" variant="default">
+      <Button className="text-sm font-semibold" size={size} variant="default">
         1
       </Button>
-      <Button className="text-sm font-semibold" size="icon-lg" variant="muted">
+      <Button className="text-sm font-semibold" size={size} variant="muted">
         2
       </Button>
-      <Button className="text-sm font-semibold" size="icon-lg" variant="muted">
+      <Button className="text-sm font-semibold" size={size} variant="muted">
         3
       </Button>
 
       <Button
         disabled
         className="pointer-events-none bg-transparent text-sm font-semibold hover:bg-transparent"
-        size="icon-lg"
+        size={size}
         variant="muted"
       >
         ...
       </Button>
 
-      <Button className="text-sm font-semibold" size="icon-lg" variant="muted">
+      <Button className="text-sm font-semibold" size={size} variant="muted">
         8
       </Button>
-      <Button className="text-sm font-semibold" size="icon-lg" variant="muted">
-        <ChevronsLeftDouble className="size-5 rotate-180" />
-      </Button>
-      <Button className="text-sm font-semibold" size="icon-lg" variant="muted">
+
+      <Button className="text-sm font-semibold" size={size} variant="muted">
         <ChevronsLeft className="size-5 rotate-180" />
       </Button>
+
+      {showHomeAndEnd ? (
+        <Button className="text-sm font-semibold" size={size} variant="muted">
+          <ChevronsLeftDouble className="size-5 rotate-180" />
+        </Button>
+      ) : null}
     </div>
   )
 }
