db28ceb0043159beb1d298d6e337777f01a44ed1
Hasan Mahmud <hasansujon786@gmail.com>
2026-02-23 10:24:38 +0600

    fix: sidebar layout responsive

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,19 +1,19 @@
-import DashboardSidebarHeader from "@/components/dashboard/Sidebar/DashboardSidebarHeader";
-import DashBoardSidebr from "@/components/dashboard/Sidebar/DashboardSidebr";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import DashboardSidebarHeader from '@/components/dashboard/Sidebar/DashboardSidebarHeader'
+import DashBoardSidebr from '@/components/dashboard/Sidebar/DashboardSidebr'
+import { SidebarProvider } from '@/components/ui/sidebar'
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <SidebarProvider>
       <DashBoardSidebr />
-      <main className="w-full">
+      <main className="w-full flex-1 md:w-[calc(100vw-var(--sidebar-width))]">
         <DashboardSidebarHeader />
-        <div className="p-6">{children}</div>
+        <div className="w-full p-4 xl:p-6">{children}</div>
       </main>
     </SidebarProvider>
-  );
+  )
 }
